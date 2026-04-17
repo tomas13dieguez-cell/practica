5e044ad6cc536a041f03d83903d0f699c693b315
tomas <tomas13dieguez@gmail.com>
2026-04-17 20:29:55 -0300

    se agregaron saludos

--- bienvenida.sh
+++ bienvenida.sh
@@ -2,3 +2,8 @@
 
 MATERIA="entorno de programacion"
 ANIO="2026"
+
+echo "====hola $USER===="
+echo "usted esta trabajando en la maquina $HOSTNAME"
+echo "usted esta trabajando en $PWD"
+echo "usted esta en $MATERIA"
